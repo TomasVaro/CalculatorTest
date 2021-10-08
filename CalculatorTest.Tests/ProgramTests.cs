dba45cc51a1ea6329d9573f4e4fafb7ebdc3dd24
tomasvaro <tomas.varo63@gmail.com>
2021-10-08 10:03:26 +0200

    Fixed InlineData with double-Array

--- CalculatorTest.Tests/ProgramTests.cs
+++ CalculatorTest.Tests/ProgramTests.cs
@@ -19,35 +19,21 @@ namespace CalculatorTest.Tests
             Assert.Equal(result, expected);
         }
 
-        [Theory]        
-        [InlineData(3.3, 4.4, 5.5, 6.6, 19.8) ]
-        [InlineData(3.3, -4.4, 5.5, 6.6, 11) ]
-        [InlineData(3.3, 0, 5.5, 6.6, 15.4) ]
-        [InlineData(3.3, 4.4, 5.5, null, 13.2)]
-        [InlineData(3.3, 4.4, 8888888888888888888, 6.6, 8888888888888888902)]
-        public void AdditionMultipleNumbersTests(double n1, double n2, double n3, double n4, double expected)
+        [Theory]
+        [InlineData(new double[] { 8, 7.5, 6, 3 }, 24.5)]
+        [InlineData(new double[] { 3.3, 4.4, 5.5, 6.6 }, 19.8)]
+        [InlineData(new double[] { 3.3, -4.4, 5.5, 6.6 }, 11)]
+        [InlineData(new double[] { 3.3, 0, 5.5, 6.6 }, 15.4)]
+        [InlineData(new double[] { 3.3, 4.4, 8888888888888888888, 6.6 }, 8888888888888888902)]
+        public void AdditionMultipleNumbersTests(double[] testNumbers, double expected)
         {
             //Arrange            
 
             //Act
-            double[] testNumbers = { n1, n2, n3, n4 };
             double result = Program.Addition(testNumbers);
             // Assert
-            Assert.Equal(result, expected, 0);
+            Assert.Equal(result, expected);
         }
-
-        //[Theory]
-        //[InlineData(new double[] { 8, 7.5, 6, 3 })]
-        //public void AdditionMultipleNumbersTest(double[] testNumbers)
-        //{
-        //    //Arrange            
-
-        //    //Act
-        //    double[] testNumbers = { n1, n2, n3, n4 };
-        //    double result = Program.Addition(testNumbers);
-        //    // Assert
-        //    Assert.Equal(result, expected, 0);
-        //}
 
 
         [Fact]
@@ -64,22 +50,20 @@ namespace CalculatorTest.Tests
         }
 
         [Theory]
-        [InlineData(33.3, 4.4, 5.5, 6.6, 16.8)]
-        [InlineData(33.3, -4.4, 5.5, 6.6, 25.6)]
-        [InlineData(33.3, 0, 5.5, 6.6, 21.2)]
-        [InlineData(33.3, 4.4, 5.5, null, 23.4)]
-        [InlineData(8888888888888, 9.5, 20.88, 30.54, 8888888888827.08)]
-        public void SubtractionMultipleNumbersTests(double n1, double n2, double n3, double n4, double expected)
+        [InlineData(new double[] { 18, 7, 6, 3 }, 2)]
+        [InlineData(new double[] { 33.3, 4.4, 5.5, 6.6 }, 16.8)]
+        [InlineData(new double[] { 33.3, -4.4, 5.5, 6.6 }, 25.6)]
+        [InlineData(new double[] { 33.3, 0, 5.5, 6.6 }, 21.2)]
+        [InlineData(new double[] { 888888888888888888, 9.5, 20.88, 30.54 }, 888888888888888827.08)]
+        public void SubtractionMultipleNumbersTests(double[] testNumbers, double expected)
         {
             //Arrange            
 
             //Act
-            double[] testNumbers = { n1, n2, n3, n4 };
             double result = Program.Subtraction(testNumbers);
             // Assert
-            Assert.Equal(result, expected, 2);
+            Assert.Equal(result, expected);
         }
-
         [Fact]
         public void MultiplicationTests()
         {
